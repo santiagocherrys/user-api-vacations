ac1cc30597af09476e670dbb97cfcd08964952c4
Santiago Echeverry <santiagocherrys@hotmail.com>
2024-12-23 12:07:47 -0500

    Adding a render-build.sh option to deploy

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,9 @@
 # exit on error
 set -o errexit
 
+# Set RAILS_ENV to production for asset precompilation
+export RAILS_ENV=production
+
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
